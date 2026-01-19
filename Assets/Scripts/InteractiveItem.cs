baef0b0188b19a449e4759dcd79c6906ca6657cf
donikusuma <donikusuma@smkrus.sch.id>
2026-01-19 07:46:06 +0700

    using ItemDataObject

--- Assets/Scripts/InteractiveItem.cs
+++ Assets/Scripts/InteractiveItem.cs
@@ -3,13 +3,19 @@ using UnityEngine.UIElements;
 
 public class InteractiveItem : MonoBehaviour
 {
-    // --- AREA INI NANTI AKAN KITA GANTI DENGAN SCRIPTABLE OBJECT ---
+    // --- SCRIPTABLE OBJECT METHOD ---
+    [SerializeField]
+    private ItemDataObject itemData;
+
+    // --- OLD HARDCODED DATA (DEPRECATED) ---
+    /*
     [Header("Hardcoded Data")]
     public string itemName;
     public enum ItemType { Potion, Coin, Trap, Fruit }
     public ItemType type;
     public int valueAmount;
-    // ----------------------------------------------------------------
+    */
+    // -----------------------------------------------
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -21,6 +27,36 @@ public class InteractiveItem : MonoBehaviour
 
     void ApplyEffect()
     {
+        // --- NEW METHOD USING SCRIPTABLE OBJECT ---
+        if (itemData == null)
+        {
+            Debug.LogWarning("ItemDataObject is not assigned!");
+            return;
+        }
+
+        switch (itemData.type)
+        {
+            case InteractType.Heal:
+                Debug.Log($"Healed for {itemData.effectValue}");
+                break;
+
+            case InteractType.Buah:
+                Debug.Log($"Got Score for {itemData.effectValue} points!");
+                break;
+
+            case InteractType.Trap:
+                Debug.Log($"Damage taken: {itemData.effectValue}");
+                break;
+        }
+
+        // Destroy object (kecuali Trap)
+        if (itemData.type != InteractType.Trap)
+        {
+            Destroy(gameObject);    
+        }
+
+        /*
+        // --- OLD HARDCODED METHOD (DEPRECATED) ---
         // TODO 5: Lengkapi Switch Case ini
         // ----- Sebelum Scriptable Object -----
 
@@ -50,5 +86,6 @@ public class InteractiveItem : MonoBehaviour
             // ...
             Destroy(gameObject);    
         }
+        */
     }
 }
